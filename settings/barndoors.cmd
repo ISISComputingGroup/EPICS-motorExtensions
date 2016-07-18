691107bf7e0bbc453da2f289e19b31e89c779196
John-Holt-Tessella <John.Holt@tessella.com>
2016-07-18 14:31:48 +0100

    Create momentum slits

--- settings/barndoors.cmd
+++ settings/barndoors.cmd
@@ -9,3 +9,8 @@ $(IFDMC01) epicsEnvSet "CALIB_DIR" "C:/Instrument/Settings/calib/barndoors"
 $(IFDMC01) dbLoadRecords("$(BARNDOORS)/db/barndoors.db", "P=$(MYPVPREFIX)MOT:,INST=EMU:,MTR=MTR0102,LOOKUP_DIR=$(CALIB_DIR),LOOKUP_READBACK_FILE=barndoors_emu_readback.txt,LOOKUP_FILE=barndoors_emu.txt,LOW_GAP_LIMIT=3.2,HIGH_GAP_LIMIT=132")
 $(IFDMC01) dbLoadRecords("$(BARNDOORS)/db/barndoors.db","P=$(MYPVPREFIX)MOT:,INST=MUSR:,MTR=MTR0103,LOOKUP_DIR=$(CALIB_DIR),LOOKUP_FILE=barndoors_musr.txt,LOOKUP_READBACK_FILE=barndoors_musr_readback.txt,LOW_GAP_LIMIT=2.7,HIGH_GAP_LIMIT=130")
 $(IFDMC01) dbLoadRecords("$(BARNDOORS)/db/barndoors.db","P=$(MYPVPREFIX)MOT:,INST=HIFI:,MTR=MTR0104,LOOKUP_DIR=$(CALIB_DIR),LOOKUP_FILE=barndoors_hifi.txt,LOOKUP_READBACK_FILE=barndoors_hifi_readback.txt,LOW_GAP_LIMIT=2.9,HIGH_GAP_LIMIT=130")
+
+
+# Define Momentum slits on first controller (if present)
+
+$(IFDMC01) dbLoadRecords("$(BARNDOORS)/db/momentum_slits.db", "P=$(MYPVPREFIX)MOT:,MTR=MTR0101,LOW_GAP_LIMIT=0,HIGH_GAP_LIMIT=100")
