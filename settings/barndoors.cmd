fd200703cb3eaa95e6c14eeff3d5b87049ce27d6
John-Holt-Tessella <John.Holt@tessella.com>
2016-08-04 17:19:18 +0100

    Move calibration data to calibration dir

--- settings/barndoors.cmd
+++ settings/barndoors.cmd
@@ -2,13 +2,14 @@
 # Create a barndoor soft motor record on top of a normal motor record which allow the gap to be set for the motor
 # Copy this file to C:\Instrument\Settings\config\<machine>\configurations\galil
 
-$(IFDMC01) epicsEnvSet "CALIB_DIR" "C:/Instrument/Settings/calib/barndoors"
+$(IFDMC01) epicsEnvSet "CALIB_BASE_DIR" "C:/Instrument/Settings/config/common"
+$(IFDMC01) epicsEnvSet "CALIB_DIR" "barndoors"
 
 # Define barndoors as soft motor records using the first galil controller, if present
 
-$(IFDMC01) dbLoadRecords("$(BARNDOORS)/db/barndoors.db", "P=$(MYPVPREFIX)MOT:,INST=EMU:,MTR=MTR0101,LOOKUP_DIR=$(CALIB_DIR),LOOKUP_READBACK_FILE=barndoors_emu_readback.txt,LOOKUP_FILE=barndoors_emu.txt,LOW_GAP_LIMIT=3.2,HIGH_GAP_LIMIT=132")
-$(IFDMC01) dbLoadRecords("$(BARNDOORS)/db/barndoors.db","P=$(MYPVPREFIX)MOT:,INST=MUSR:,MTR=MTR0102,LOOKUP_DIR=$(CALIB_DIR),LOOKUP_FILE=barndoors_musr.txt,LOOKUP_READBACK_FILE=barndoors_musr_readback.txt,LOW_GAP_LIMIT=2.7,HIGH_GAP_LIMIT=130")
-$(IFDMC01) dbLoadRecords("$(BARNDOORS)/db/barndoors.db","P=$(MYPVPREFIX)MOT:,INST=HIFI:,MTR=MTR0103,LOOKUP_DIR=$(CALIB_DIR),LOOKUP_FILE=barndoors_hifi.txt,LOOKUP_READBACK_FILE=barndoors_hifi_readback.txt,LOW_GAP_LIMIT=2.9,HIGH_GAP_LIMIT=130")
+$(IFDMC01) dbLoadRecords("$(BARNDOORS)/db/barndoors.db", "P=$(MYPVPREFIX)MOT:,INST=EMU:,MTR=MTR0101,CALIB_BASE_DIR=$(CALIB_BASE_DIR),CALIB_DIR=$(CALIB_DIR),CALIB_FILE=barndoors_emu.txt,CALIB_READBACK_FILE=barndoors_emu_readback.txt,LOW_GAP_LIMIT=3.2,HIGH_GAP_LIMIT=132")
+$(IFDMC01) dbLoadRecords("$(BARNDOORS)/db/barndoors.db","P=$(MYPVPREFIX)MOT:,INST=MUSR:,MTR=MTR0102,CALIB_BASE_DIR=$(CALIB_BASE_DIR),CALIB_DIR=$(CALIB_DIR),CALIB_FILE=barndoors_musr.txt,CALIB_READBACK_FILE=barndoors_musr_readback.txt,LOW_GAP_LIMIT=2.7,HIGH_GAP_LIMIT=130")
+$(IFDMC01) dbLoadRecords("$(BARNDOORS)/db/barndoors.db","P=$(MYPVPREFIX)MOT:,INST=HIFI:,MTR=MTR0103,CALIB_BASE_DIR=$(CALIB_BASE_DIR),CALIB_DIR=$(CALIB_DIR),CALIB_FILE=barndoors_hifi.txt,CALIB_READBACK_FILE=barndoors_hifi_readback.txt,LOW_GAP_LIMIT=2.9,HIGH_GAP_LIMIT=130")
 
 
 # Define momentum slits as a soft motor record using the first galil controller, if present
