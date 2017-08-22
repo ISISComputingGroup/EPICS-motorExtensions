6a2d3aa036fdbe7875c3ad0ce486411e3e5a1d43
Samuel Jackson <samuel.jackson@stfc.ac.uk>
2017-08-22 11:31:27 +0100

    Make settings path consistent

--- settings/motorExtensions.cmd
+++ settings/motorExtensions.cmd
@@ -2,6 +2,6 @@
 # Create a soft motor record on top of a normal motor record which allow the gap to be set for the motor
 # Copy this file to C:\Instrument\Settings\config\<machine>\configurations\galil
 
-$(IFNOTTESTDEVSIM) < $(GALILCONFIG)/xyBeamstop.cmd
+$(IFNOTTESTDEVSIM) < $(GALILCONFIG)/settings/xyBeamstop/xyBeamstop.cmd
 $(IFTESTDEVSIM) < $(MOTOREXT)/settings/xyBeamstop/xyBeamstop.cmd
 
