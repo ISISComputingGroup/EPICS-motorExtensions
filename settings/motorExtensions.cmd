7abd934bf964d00f1adeb5c17c4ad1d3615e0bd3
AdrianPotter <adrian.potter@tessella.com>
2017-09-14 11:39:45 +0100

    Update README on how to use xybeamstop

--- settings/motorExtensions.cmd
+++ settings/motorExtensions.cmd
@@ -2,6 +2,6 @@
 # Create a soft motor record on top of a normal motor record which allow the gap to be set for the motor
 # Copy this file to C:\Instrument\Settings\config\<machine>\configurations\galil
 
-$(IFNOTTESTDEVSIM) < $(GALILCONFIG)/settings/xyBeamstop/xyBeamstop.cmd
+$(IFNOTTESTDEVSIM) < $(GALILCONFIG)/xyBeamstop/xyBeamstop.cmd
 $(IFTESTDEVSIM) < $(MOTOREXT)/settings/xyBeamstop/xyBeamstop.cmd
 
