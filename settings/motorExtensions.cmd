335ef528b99994e7d4f9d4cad2b5324f5c3ec436
AdrianPotter <adrian.potter@tessella.com>
2017-09-15 11:40:39 +0100

    xyBeamstop should be in the parent galil directory

--- settings/motorExtensions.cmd
+++ settings/motorExtensions.cmd
@@ -2,6 +2,6 @@
 # Create a soft motor record on top of a normal motor record which allow the gap to be set for the motor
 # Copy this file to C:\Instrument\Settings\config\<machine>\configurations\galil
 
-$(IFNOTTESTDEVSIM) < $(GALILCONFIG)/xyBeamstop/xyBeamstop.cmd
+$(IFNOTTESTDEVSIM) < $(GALILCONFIG)/xyBeamstop.cmd
 $(IFTESTDEVSIM) < $(MOTOREXT)/settings/xyBeamstop/xyBeamstop.cmd
 
