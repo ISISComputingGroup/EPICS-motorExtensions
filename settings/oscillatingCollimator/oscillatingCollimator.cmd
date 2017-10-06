2c7331cfddbcbcd7cb413dbe7ccda2471f8babc1
AdrianPotter <adrian.potter@tessella.com>
2017-10-06 12:21:00 +0100

    Lots of tweaks and a README. This state successfully starts/runs/stops the program on the G39 Galil

--- settings/oscillatingCollimator/oscillatingCollimator.cmd
+++ settings/oscillatingCollimator/oscillatingCollimator.cmd
@@ -1,3 +1,10 @@
 ############ Oscillating Collimator ############
 
-dbLoadRecords("$(MOTOREXT)/db/oscillatingCollimator.db", "P=$(MYPVPREFIX)MOT:, O=OSCCOL:, M=MTR0101:")
+epicsEnvSet "CONTROLLER_NUMBER" "1"
+epicsEnvSet "MOTOR_NUMBER" "3"
+epicsEnvSet "COLLIMATOR_PV_PREFIX" "OSCCOL"
+
+calc("CNUM_PADDED", "$(CONTROLLER_NUMBER=1)", 2, 2)
+calc("MNUM_PADDED", "$(MOTOR_NUMBER=1)", 2, 2)
+
+dbLoadRecords("$(MOTOREXT)/db/oscillatingCollimator.db", "P=$(MYPVPREFIX)MOT:, O=$(COLLIMATOR_PV_PREFIX), M=MTR$(CNUM_PADDED)$(MNUM_PADDED), D=DMC$(CNUM_PADDED), AXIS=$(MOTOR_NUMBER)")
