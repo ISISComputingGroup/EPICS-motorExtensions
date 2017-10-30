03e785c90815e2679eb03b084be618b395deab0b
Adrian Potter <Adrian.Potter@tessella.com>
2017-10-30 16:49:05 +0000

    Add missing character

--- settings/oscillatingCollimator/motorExtensions.cmd
+++ settings/oscillatingCollimator/motorExtensions.cmd
@@ -1,5 +1,5 @@
 ############ Motor Extensions ##############
-# Create a soft motor record on top of a normal motor record which allow the gap to be set for the motor
+# Create a soft motor record on top of a normal motor record
 # Copy this file to C:\Instrument\Settings\config\<machine>\configurations\galil
 
-$(GALILCONFIG)/oscillatingCollimator.cmd
+< $(GALILCONFIG)/oscillatingCollimator.cmd
