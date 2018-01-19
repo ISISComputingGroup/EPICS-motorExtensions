5c7b3e4eb3be5427ab02f8b0b1999987954f05b5
Dominic Oram <dominic.oram@stfc.ac.uk>
2018-01-19 11:58:14 +0000

    Added recsim for the beamscapper jaws

--- settings/gem_jaws/jaws.cmd
+++ settings/gem_jaws/jaws.cmd
@@ -6,4 +6,4 @@
 $(IFIOC_LINMOT_01) dbLoadRecords("$(MOTOREXT)/db/gemjawset.db", "P=$(MYPVPREFIX)MOT:, JAWS=JAWS6:, N_MTR=MTR0101, S_MTR=MTR0102, W_MTR=MTR0103, E_MTR=MTR0104")
 
 # Define a jawset based on this calibrated axis
-$(IFIOC_LINMOT_01) dbLoadRecords("$(JAWS)/db/jaws.db","P=$(MYPVPREFIX)MOT:, JAWS=JAWS6:, mXN=JAWS6:CAL_N, mXS=JAWS6:CAL_S, mXW=JAWS6:CAL_W, mXE=JAWS6:CAL_E")
+#$(IFIOC_LINMOT_01) dbLoadRecords("$(JAWS)/db/jaws.db","P=$(MYPVPREFIX)MOT:, JAWS=JAWS6:, mXN=JAWS6:CAL_N, mXS=JAWS6:CAL_S, mXW=JAWS6:CAL_W, mXE=JAWS6:CAL_E")
