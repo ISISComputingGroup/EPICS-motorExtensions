7974d0fbbd17836f14d3e40026970c57abb0ab3c
Tom Willemsen <tomwillemsen1995@gmail.com>
2018-03-08 13:05:40 +0000

    Update jaw scale factors to new values

--- settings/gem_jaws/jaws.cmd
+++ settings/gem_jaws/jaws.cmd
@@ -12,4 +12,4 @@ $(IFIOC_GALIL_01=#) dbLoadRecords("$(JAWS)/db/jaws.db","P=$(MYPVPREFIX)MOT:,JAWS
 $(IFIOC_GALIL_02=#) dbLoadRecords("$(JAWS)/db/jaws.db","P=$(MYPVPREFIX)MOT:,JAWS=JAWS3:,mXN=MTR0204,mXS=MTR0203,mXW=MTR0202,mXE=MTR0201")
 $(IFIOC_GALIL_02=#) dbLoadRecords("$(JAWS)/db/jaws.db","P=$(MYPVPREFIX)MOT:,JAWS=JAWS4:,mXN=MTR0208,mXS=MTR0207,mXW=MTR0206,mXE=MTR0205")
 $(IFIOC_GALIL_03=#) dbLoadRecords("$(JAWS)/db/jaws.db","P=$(MYPVPREFIX)MOT:,JAWS=JAWS5:,mXN=MTR0301,mXS=MTR0303,mXW=MTR0304,mXE=MTR0302")
-$(IFIOC_GALIL_01=#) dbLoadRecords("C:/Instrument/Apps/EPICS/support/motorExtensions/master/db/gem_beamline_jawset.db", "P=$(MYPVPREFIX),J1_SCALE=0.627,J2_SCALE=0.521,J3_SCALE=0.396,J4_SCALE=0.2545,J5_SCALE=0.097")
+$(IFIOC_GALIL_01=#) dbLoadRecords("C:/Instrument/Apps/EPICS/support/motorExtensions/master/db/gem_beamline_jawset.db", "P=$(MYPVPREFIX),J1_SCALE=0.6281,J2_SCALE=0.5219,J3_SCALE=0.3924,J4_SCALE=0.2563,J5_SCALE=0.0947")
