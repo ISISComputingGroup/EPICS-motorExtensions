46a9c66788b641e54dd1e52d8736575e4c64f6bb
Tom Willemsen <tomwillemsen1995@gmail.com>
2018-09-17 16:18:02 +0100

    Make use mclennan rather than galil

--- settings/toscaSampleChanger/motorExtensions.cmd
+++ settings/toscaSampleChanger/motorExtensions.cmd
@@ -2,4 +2,4 @@
 # Create a soft motor record on top of a normal motor record
 # Copy this file to C:\Instrument\Settings\config\<machine>\configurations\galil
 
-< $(GALILCONFIG)/toscaSampleChanger.cmd
+< $(MCLENCONFIG)/toscaSampleChanger.cmd
