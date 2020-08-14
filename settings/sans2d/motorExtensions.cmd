01aa75dbe207e117b4da1279ef303882cc02e4c2
Alexandru Dascalu <965337@swansea.ac.uk>
2020-08-14 19:03:41 +0100

    changed test settings

--- settings/sans2d/motorExtensions.cmd
+++ settings/sans2d/motorExtensions.cmd
@@ -1,4 +1,4 @@
 ##### SANS2D Vacuum tank motor extensions #####
 # Copy this file to C:\Instrument\Settings\config\<machine>\configurations\galil
 
-$(IFIOC_GALIL_05) dbLoadRecords("$(MOTOREXT)/db/sans2d_vac_tank.db", "P=$(MYPVPREFIX)MOT:")
+$(IFIOC_GALIL_05) dbLoadRecords("$(MOTOREXT)/db/sans2d_vac_tank.db", "P=$(MYPVPREFIX)MOT:,SANS2D_VACUUM_TANK_INTERVAL_MINOR_WARNING=100.0,SANS2D_VACUUM_TANK_INTERVAL_MAJOR_WARNING=50.0")
