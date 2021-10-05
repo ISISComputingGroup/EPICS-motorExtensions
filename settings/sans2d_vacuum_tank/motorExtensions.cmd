888931c989c0e778779e3bc425620cc39babc9ac
bishal <bishal.rai@stfc.ac.uk>
2021-10-05 13:32:57 +0100

    changes for IOC test

--- settings/sans2d_vacuum_tank/motorExtensions.cmd
+++ settings/sans2d_vacuum_tank/motorExtensions.cmd
@@ -1,6 +1,6 @@
 ##### SANS2D Vacuum tank motor extensions #####
 # Copy this file to C:\Instrument\Settings\config\<machine>\configurations\galil
 
-$(IFIOC_GALIL_05) dbLoadRecords("$(MOTOREXT)/db/sans2d_vac_tank.db", "P=$(MYPVPREFIX)MOT:,SANS2D_VACUUM_TANK_INTERVAL_MINOR_WARNING=100.0,SANS2D_VACUUM_TANK_INTERVAL_MAJOR_WARNING=50.0")
+$(IFIOC_GALILMUL_02) dbLoadRecords("$(MOTOREXT)/db/sans2d_vac_tank.db", "P=$(MYPVPREFIX)MOT:,SANS2D_VACUUM_TANK_INTERVAL_MINOR_WARNING=100.0,SANS2D_VACUUM_TANK_INTERVAL_MAJOR_WARNING=50.0")
 
 $(IFIOC_GALILMUL_01) dbLoadRecords("$(MOTOREXT)/db/sans2d_apertures_waveguides.db", "P=$(MYPVPREFIX)MOT:,MYPVPREFIX=$(MYPVPREFIX)")
