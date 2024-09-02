5d6f7efb715dcb3b4f2070236c5739b607bb6c95
Tom Willemsen <tom.willemsen@stfc.ac.uk>
2024-09-02 13:38:35 +0100

    Add INTER settings

---
+++ settings/INTER/twincat/motorExtensions.cmd
@@ -0,0 +1,4 @@
+##### INTER Beckhoff motor extensions #####
+# Copy this file to C:\Instrument\Settings\config\<machine>\configurations\twincat
+
+$(IFIOC_TC_01) dbLoadRecords("$(MOTOREXT)/db/inter_tank.db", "P=$(MYPVPREFIX)TC_01:,PORT=$(PORT),ADSPORT=$(ADSPORT=852)")
