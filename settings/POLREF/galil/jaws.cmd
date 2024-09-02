910bd560f4676eb9b54dcff80d805fa02d224560
Tom Willemsen <tom.willemsen@stfc.ac.uk>
2024-09-02 13:41:42 +0100

    Add POLREF settings

---
+++ settings/POLREF/galil/jaws.cmd
@@ -0,0 +1,11 @@
+############ POLREF jaws ##############
+# Create a soft motor record on top of a normal motor record which allow the gap to be set for the motor
+# Copy this file to C:\Instrument\Settings\config\<machine>\configurations\linmot AND C:\Instrument\Settings\config\<machine>\configurations\galil
+
+# Beamline jaws on GALIL_01, GALIL_02, GALIL_03
+
+$(IFIOC_GALIL_01=#) dbLoadRecords("$(JAWS)/db/jaws.db","P=$(MYPVPREFIX)MOT:,JAWS=JAWS0:,mXN=MTR0101,mXS=MTR0103,mXW=MTR0104,mXE=MTR0102,IFINIT_FROM_AS=$(IFINIT_JAWS_FROM_AS=#),IFNOTINIT_FROM_AS=$(IFNOTINIT_JAWS_FROM_AS=)")
+$(IFIOC_GALIL_02=#) dbLoadRecords("$(JAWS)/db/jaws.db","P=$(MYPVPREFIX)MOT:,JAWS=JAWS1:,mXN=MTR0202,mXS=MTR0201,mXW=MTR0203,mXE=MTR0204,IFINIT_FROM_AS=$(IFINIT_JAWS_FROM_AS=#),IFNOTINIT_FROM_AS=$(IFNOTINIT_JAWS_FROM_AS=)")
+$(IFIOC_GALIL_03=#) dbLoadRecords("$(JAWS)/db/jaws.db","P=$(MYPVPREFIX)MOT:,JAWS=JAWS2:,mXN=MTR0308,mXS=MTR0307,mXW=MTR0306,mXE=MTR0305,IFINIT_FROM_AS=$(IFINIT_JAWS_FROM_AS=#),IFNOTINIT_FROM_AS=$(IFNOTINIT_JAWS_FROM_AS=)")
+$(IFIOC_GALIL_06=#) dbLoadRecords("$(JAWS)/db/jaws.db","P=$(MYPVPREFIX)MOT:,JAWS=JAWS3:,mXN=MTR0602,mXS=MTR0601,mXW=MTR0604,mXE=MTR0603,IFINIT_FROM_AS=$(IFINIT_JAWS_FROM_AS=#),IFNOTINIT_FROM_AS=$(IFNOTINIT_JAWS_FROM_AS=)")
+$(IFIOC_GALIL_08=#) dbLoadRecords("$(JAWS)/db/jaws.db","P=$(MYPVPREFIX)MOT:,JAWS=JAWS4:,mXN=MTR0806,mXS=MTR0805,mXW=MTR0807,mXE=MTR0808,IFINIT_FROM_AS=$(IFINIT_JAWS_FROM_AS=#),IFNOTINIT_FROM_AS=$(IFNOTINIT_JAWS_FROM_AS=)")
