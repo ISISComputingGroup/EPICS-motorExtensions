f84251fb8a0c73924a73fc78ae81ef5696952f7b
Tom Willemsen <tom.willemsen@stfc.ac.uk>
2024-09-03 14:57:16 +0100

    Remove reference to ZOOM bumpstrip from OSIRIS

--- settings/OSIRIS/galil/motorExtensions.cmd
+++
@@ -1,5 +0,0 @@
-############ Motor Extensions ##############
-# Create a soft motor record on top of a normal motor record
-# Copy this file to C:\Instrument\Settings\config\<machine>\configurations\galil
-
-# < $(GALILCONFIG)/zoom_bumpstrip.cmd
