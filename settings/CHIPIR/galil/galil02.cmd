c8149d708abb41de65f3edecdbc29946d52cca2c
Jack Harper <jack.harper@stfc.ac.uk>
2024-10-22 16:11:20 +0100

    backing up working settings on chipir

--- settings/CHIPIR/galil/galil02.cmd
+++ settings/CHIPIR/galil/galil02.cmd
@@ -21,7 +21,7 @@ GalilCreateAxis("Galil","H",1,"",1)
 
 epicsEnvSet "HOME_HEADER" "$(GALIL)/gmc/galil_Default_Header.gmc"
 epicsEnvSet "HOME_MODE_1" "$(GALIL)/gmc/galil_Home_Dummy_Do_Nothing.gmc"
-epicsEnvSet "HOME_MODE_2" "$(GALIL)/gmc/galil_Home_ForwLimit.gmc"
+epicsEnvSet "HOME_MODE_2" "$(GALIL)/gmc/galil_Home_JogForwLimit.gmc"
 epicsEnvSet "HOME_MODE_3" "$(GALIL)/gmc/galil_Home_ForwLimit.gmc"
 epicsEnvSet "HOME_MODE_4" "$(GALIL)/gmc/galil_Home_ForwLimit.gmc"
 epicsEnvSet "HOME_MODE_5" "$(GALIL)/gmc/galil_Home_ForwLimit.gmc"
@@ -29,8 +29,8 @@ epicsEnvSet "HOME_MODE_6" "$(GALIL)/gmc/galil_Home_ForwLimit.gmc"
 epicsEnvSet "HOME_MODE_7" "$(GALIL)/gmc/galil_Home_Dummy_Do_Nothing.gmc"
 epicsEnvSet "HOME_MODE_8" "$(GALIL)/gmc/galil_Home_Dummy_Do_Nothing.gmc"
 epicsEnvSet "HOME_FOOTER" "$(GALIL)/gmc/galil_Default_Footer.gmc"
-epicsEnvSet "COL" "$(GALIL)/gmc/galil_CHIPIR_Collimator.gmc"  # TODO: removed below
+epicsEnvSet "COL" "$(GALIL)/gmc/galil_CHIPIR_Collimator.gmc"
 
-GalilStartController("Galil","$(HOME_HEADER);$(HOME_MODE_1)!$(HOME_MODE_2)!$(HOME_MODE_3)!$(HOME_MODE_4)!$(HOME_MODE_5)!$(HOME_MODE_6)!$(HOME_MODE_7)!$(HOME_MODE_8);$(HOME_FOOTER)",0,0,3)
+GalilStartController("Galil","$(HOME_HEADER)!$(COL);$(HOME_MODE_1)!$(HOME_MODE_2)!$(HOME_MODE_3)!$(HOME_MODE_4)!$(HOME_MODE_5)!$(HOME_MODE_6)!$(HOME_MODE_7)!$(HOME_MODE_8);$(HOME_FOOTER)",0,0,3)
 
 # Ensure there is a newline at the end of the file!
