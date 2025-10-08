1c8dc1ce165bee726d830369e56d941a0eb0d380
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2025-10-08 16:10:22 +0100

    Update sampleChanger.cmd
    unclear if needed, i've deleted them for now

--- settings/IRIS/mclennan/sampleChanger.cmd
+++ settings/IRIS/mclennan/sampleChanger.cmd
@@ -1,12 +1,3 @@
-## commented out as local xml/txt files do not seem to exist?
-#$(IFAXIS1) epicsEnvSet "RACKDEFS" "$(ICPCONFIGROOT)/motionSetPoints/rack_definitions.xml"
-#$(IFAXIS1) epicsEnvSet "SLOT_DETAILS_FILE" "$(ICPCONFIGROOT)/motionSetPoints/samplechanger.xml"
-#$(IFAXIS1) epicsEnvSet "SAMPLE_LKUP_FILE" "$(ICPCONFIGROOT)/motionSetPoints/sample.txt"
-
-#$(IFAXIS1) sampleChangerConfigure("CURRENT","CURRENT",2)
-
-#$(IFAXIS1) dbLoadRecords("$(SAMPLECHANGER)/db/sampleChanger.db","P=$(MYPVPREFIX)SAMPCHNG:,MOT_SET_POINT=$(MYPVPREFIX)LKUP:SAMPLE:,LOOKUP=CURRENT")
-
 $(IFAXIS1) epicsEnvSet "RACKDEFS" "$(ICPCONFIGROOT)/motionSetPoints/lin_rack_definitions.xml"
 $(IFAXIS1) epicsEnvSet "SLOT_DETAILS_FILE" "$(ICPCONFIGROOT)/motionSetPoints/linearsamplechanger.xml"
 $(IFAXIS1) epicsEnvSet "SAMPLE_LKUP_FILE" "$(ICPCONFIGROOT)/motionSetPoints/linsample.txt"
