d79dbd264b5ec74ea26d9de561d1405b4f2486c8
Tom Willemsen <tom.willemsen@stfc.ac.uk>
2026-04-23 14:28:27 +0100

    Commission replacement MAPS sample jaws

--- settings/MAPS/galil/jaws.cmd
+++ settings/MAPS/galil/jaws.cmd
@@ -2,4 +2,4 @@
 # Create a soft motor record on top of a normal motor record which allow the gap to be set for the motor
 # Copy this file to C:\Instrument\Settings\config\<machine>\configurations\linmot
 
-$(IFMTRCTRL1) dbLoadRecords("$(JAWS)/db/jaws.db","P=$(MYPVPREFIX)MOT:,JAWS=JAWS1:,mXN=MTR0101,mXS=MTR0102,mXE=MTR0103,mXW=MTR0104,IFINIT_FROM_AS=$(IFINIT_JAWS_FROM_AS=#),IFNOTINIT_FROM_AS=$(IFNOTINIT_JAWS_FROM_AS=)")
+$(IFIOC_GALIL_02) dbLoadRecords("$(JAWS)/db/jaws.db","P=$(MYPVPREFIX)MOT:,JAWS=JAWS2:,mXN=MTR0201,mXS=MTR0202,mXE=MTR0204,mXW=MTR0203,IFINIT_FROM_AS=$(IFINIT_JAWS_FROM_AS=#),IFNOTINIT_FROM_AS=$(IFNOTINIT_JAWS_FROM_AS=)")
