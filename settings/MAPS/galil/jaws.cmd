a558fe45b68c43c42a349cddb4787462de516a20
Tom Willemsen <tomwillemsen1995@gmail.com>
2026-04-25 08:10:32 +0000

    Remove comments from jaws.cmd

--- settings/MAPS/galil/jaws.cmd
+++ settings/MAPS/galil/jaws.cmd
@@ -1,5 +1,1 @@
-############ MAPS jaws ##############
-# Create a soft motor record on top of a normal motor record which allow the gap to be set for the motor
-# Copy this file to C:\Instrument\Settings\config\<machine>\configurations\linmot
-
 $(IFIOC_GALIL_02) dbLoadRecords("$(JAWS)/db/jaws.db","P=$(MYPVPREFIX)MOT:,JAWS=JAWS2:,mXN=MTR0201,mXS=MTR0202,mXE=MTR0204,mXW=MTR0203,IFINIT_FROM_AS=$(IFINIT_JAWS_FROM_AS=#),IFNOTINIT_FROM_AS=$(IFNOTINIT_JAWS_FROM_AS=)")
